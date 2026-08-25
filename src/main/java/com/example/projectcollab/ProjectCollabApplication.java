3a9905d680a4824ec4e62950c173727b51fa0250
changha <yullraes@gmail.com>
2026-08-25 15:07:05 +0900

    chore(repo): initialize project repository

---
+++ src/main/java/com/example/projectcollab/ProjectCollabApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectcollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCollabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCollabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectcollab/ProjectCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectcollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCollabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
